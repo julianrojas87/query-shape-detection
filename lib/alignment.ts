4da0908efea9a66c4c1fa93d1fdd97f4e52ddaeb
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2023-09-27 13:44:40 +0200

    find the property function created

--- lib/alignment.ts
+++ lib/alignment.ts
@@ -1,8 +1,37 @@
+import { Algebra, translate, Factory as AlgebraFactory, Util } from 'sparqlalgebrajs';
+import { PropertyObject } from './types';
+import { Term } from "@rdfjs/types"
 
-export function test(): string {
-    const msg = "Bonjour l'univers";
-    console.log(msg);
-    return msg;
+export function findIriPropertiesWithObject(query: string): PropertyObject[] {
+    const resp: PropertyObject[] = [];
+    const algebraQuery = translate(query);
+    if (algebraQuery === undefined) {
+        throw new Error("the query is not valid");
+    }
+
+    const addProperty = (quad: any): boolean => {
+        if (quad.predicate === undefined || quad.object === undefined) {
+            throw new Error("the query is not valid path or patterns does not have triples");
+        }
+        const predicate = quad.predicate as Term;
+        const object = quad.object as Term;
+        if (predicate.termType === "NamedNode") {
+            const propertyIri = quad.predicate.value;
+            resp.push({
+                property_iri: propertyIri,
+                object
+            });
+        }
+
+        return true;
+    };
+
+    Util.recurseOperation(
+        algebraQuery,
+        {
+            [Algebra.types.PATH]: addProperty,
+            [Algebra.types.PATTERN]: addProperty,
+        }
+    );
+    return resp;
 }
-
-
--- test/alignment-test.ts
+++ test/alignment-test.ts
@@ -1,9 +1,53 @@
 import { describe } from 'node:test';
-import { test } from '../lib/alignment';
+import { findIriPropertiesWithObject } from '../lib/alignment';
 
 
 describe('aligment', () => {
-    it('should be deleted when real test will be created ;)', () => {
-        expect(test()).toBe("Bonjour l'univers");
-    })
+
+    describe('findIriPropertiesWithObject', ()=>{
+        it('should return the property with an IRI given a query with one triple', ()=>{
+            const query = "SELECT * WHERE { ?x <http://exemple.ca> ?z }";
+            const resp = findIriPropertiesWithObject(query);
+            expect(resp.length).toBe(1);
+            expect(resp[0].property_iri).toBe("http://exemple.ca");
+            expect(resp[0].object.termType).toBe("Variable");
+            expect(resp[0].object.value).toBe("z");
+        });
+
+        it('should return no property given a query with one triple', ()=>{
+            const query = "SELECT * WHERE { ?x ?o ?z }";
+            const resp = findIriPropertiesWithObject(query);
+            expect(resp.length).toBe(0);
+        });
+
+        it('should the properties with a query with multiple triples', ()=>{
+            const query = `SELECT * WHERE { 
+                ?x ?o ?z .
+                ?x <http://exemple.ca> ?z .
+                ?z <http://exemple.be> "abc" .
+                ?w <http://exemple.be> <http://objet.fr> .
+                <http://sujet.cm> <http://predicat.cm> "def" .
+                <http://sujet.cm> ?m "def" .
+            }`;
+            const resp = findIriPropertiesWithObject(query);
+            expect(resp.length).toBe(4);
+            
+            expect(resp[0].property_iri).toBe("http://exemple.ca");
+            expect(resp[0].object.termType).toBe("Variable");
+            expect(resp[0].object.value).toBe("z");
+
+            expect(resp[1].property_iri).toBe("http://exemple.be");
+            expect(resp[1].object.termType).toBe("Literal");
+            expect(resp[1].object.value).toBe("abc");
+
+            expect(resp[2].property_iri).toBe("http://exemple.be");
+            expect(resp[2].object.termType).toBe("NamedNode");
+            expect(resp[2].object.value).toBe("http://objet.fr");
+
+            expect(resp[3].property_iri).toBe("http://predicat.cm");
+            expect(resp[3].object.termType).toBe("Literal");
+            expect(resp[3].object.value).toBe("def");
+        });
+    });
+
 });
