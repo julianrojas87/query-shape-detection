210b5dbef9188a733f0bb2d02e0336b98fec396e
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-02-23 14:42:42 +0100

    hacky way to get the shape

--- lib/shex.ts
+++ lib/shex.ts
@@ -1,5 +1,7 @@
 import { SimpleShape } from './types';
 import * as ShEx from 'shexj';
+import * as RDF from '@rdfjs/types';
+import { SHEX_PREDICATE, SHEX_EXPRESSION } from './constant';
 
 const shexParser = require('@shexjs/parser');
 const shexVisitor = require('@shexjs/visitor').Visitor;
@@ -7,24 +9,24 @@ const shexVisitor = require('@shexjs/visitor').Visitor;
 const visitor = shexVisitor();
 
 /**
- * 
- * @param input_schema 
- * @param shape_iri 
- * @returns 
+ * get all the shapes from a schema
+ * @param {string|ShEx.Schema} input_schema 
+ * @param {string} shape_iri 
+ * @returns {Map<string, ShEx.ShapeDecl> | Error | undefined} a map of shapes, an error if it was not able to be parsed or undefined if there is no shapes
  */
-export function getAllShapes(input_schema: string|ShEx.Schema, shape_iri: string): Map<string, ShEx.ShapeDecl> | Error | undefined {
-    const parser = shexParser.construct(shape_iri);
+export function getAllShapes(input_schema: string | ShEx.Schema, shape_iri: string): Map<string, ShEx.ShapeDecl> | Error | undefined {
     let schema: ShEx.Schema;
-    if (typeof input_schema  === 'string'){
+    if (typeof input_schema === 'string') {
         try {
+            const parser = shexParser.construct(shape_iri);
             schema = parser.parse(input_schema);
-        } catch (error:any) {
-            return <Error> error
+        } catch (error: any) {
+            return <Error>error
         }
-    }else{
+    } else {
         schema = input_schema;
     }
-    
+
 
     const shapes: ShEx.ShapeDecl[] | undefined = schema.shapes;
     if (shapes === undefined || shapes.length === 0) {
@@ -39,7 +41,49 @@ export function getAllShapes(input_schema: string|ShEx.Schema, shape_iri: string
 
     return shape_map;
 }
+export function hackCreateSimpleShapesFromQuadStream(streamingQuads: RDF.Stream<RDF.Quad>): Promise<SimpleShape> {
+    const predicates: string[] = [];
+    let name: string | undefined;
+    let multipleShapes = false;
+    return new Promise((resolve, reject) => {
+        streamingQuads.on('data', (quad: RDF.Quad) => {
+            if (quad.predicate.equals(SHEX_PREDICATE)) {
+                predicates.push(quad.object.value);
+            }
+            if (quad.predicate.equals(SHEX_EXPRESSION)) {
+                if (name !== undefined) {
+                    multipleShapes = true;
+                }
+                name = quad.subject.value;
+            }
+        });
 
+        streamingQuads.on('error', (error) => {
+            reject(error);
+        })
+        streamingQuads.on('end', () => {
+            if (multipleShapes === true) {
+                reject(new Error("there is multiple shapes"));
+                return;
+            }
+            if (name === undefined) {
+                reject(new Error("there is no shex expression defined"));
+                return;
+            }
+            resolve({
+                name,
+                predicates
+            }
+            )
+        });
+    });
+}
+
+/**
+ * create a map of simple shapes
+ * @param {Map<string, ShEx.ShapeDecl>} shapes - ShEx shapes
+ * @returns {Map<string, SimpleShape>} map of simple shapes
+ */
 export function createSimpleShapes(shapes: Map<string, ShEx.ShapeDecl>): Map<string, SimpleShape> {
     const simple_shapes: Map<string, SimpleShape> = new Map();
     for (const [key, shape] of shapes.entries()) {
@@ -49,7 +93,7 @@ export function createSimpleShapes(shapes: Map<string, ShEx.ShapeDecl>): Map<str
         };
         visitor.visitShapeDecl(shape);
 
-        simple_shapes.set(key, { name: key, predicates: shape_predicates});
+        simple_shapes.set(key, { name: key, predicates: shape_predicates });
     }
     return simple_shapes;
 }
