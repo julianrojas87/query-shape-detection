9dbaac95cee4a0cb77b3f3a593f494e8496a5199
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-03-18 12:50:03 +0100

    support for path queries

--- lib/query.ts
+++ lib/query.ts
@@ -1,15 +1,18 @@
-import type { Term } from '@rdfjs/types';
+import type { Term, BaseQuad} from '@rdfjs/types';
 import { Algebra, translate, Util } from 'sparqlalgebrajs';
 import type { IPropertyObject } from './aligment';
 import { PropertyObject } from './aligment';
+import { DataFactory } from 'rdf-data-factory';
+
+const DF = new DataFactory<BaseQuad>();
 
 export function createSimplePropertyObjectFromQuery(query: string): IPropertyObject[] {
   const resp: IPropertyObject[] = [];
   const algebraQuery = translate(query);
 
   const addProperty = (quad: any): boolean => {
-    const predicate = <Term> quad.predicate;
-    const object = <Term> quad.object;
+    const predicate = <Term>quad.predicate;
+    const object = <Term>quad.object;
     if (predicate.termType === 'NamedNode') {
       const propertyIri = quad.predicate.value;
       resp.push(new PropertyObject(
@@ -21,11 +24,29 @@ export function createSimplePropertyObjectFromQuery(query: string): IPropertyObj
     return true;
   };
 
+  const addPropertySeq = (element: any): boolean => {
+    const quadArray = <Term[]>((<any[]>element.input).map((value) => {
+      return value.iri;
+    }));
+    for (const predicate of quadArray) {
+      if (predicate.termType === 'NamedNode') {
+        resp.push(new PropertyObject(
+          predicate.value,
+          // for the moment we will simply ignore this
+          DF.blankNode(),
+        ));
+      }
+
+    }
+    return true;
+  }
+
   Util.recurseOperation(
     algebraQuery,
     {
       [Algebra.types.PATH]: addProperty,
       [Algebra.types.PATTERN]: addProperty,
+      [Algebra.types.SEQ]: addPropertySeq,
     },
   );
   return resp;
--- test/query-test.ts
+++ test/query-test.ts
@@ -1,4 +1,8 @@
 import { createSimplePropertyObjectFromQuery } from '../lib/query';
+import type { Term, BaseQuad } from '@rdfjs/types';
+import { DataFactory } from 'rdf-data-factory';
+
+const DF = new DataFactory<BaseQuad>();
 
 describe('query', () => {
   describe('createSimplePropertyObjectFromQuery', () => {
@@ -44,6 +48,62 @@ describe('query', () => {
       expect(resp[3].property_iri).toBe('http://predicat.cm');
       expect(resp[3].object.termType).toBe('Literal');
       expect(resp[3].object.value).toBe('def');
+    });
+
+    it('should the properties with a complex query', () => {
+      const query = `PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+      PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+      PREFIX snvoc: <http://solidbench-server:3000/www.ldbc.eu/ldbc_socialnet/1.0/vocabulary/>
+      SELECT ?commentId ?commentContent ?commentCreationDate ?replyAuthorId ?replyAuthorFirstName ?replyAuthorLastName ?replyAuthorKnowsOriginalMessageAuthor WHERE {
+        <http://solidbench-server:3000/pods/00000002199023256081/comments/Philippines#274878069404> snvoc:id ?messageId;
+          snvoc:hasCreator ?messageCreator.
+        ?messageCreator snvoc:id ?messageCreatorId.
+        ?comment snvoc:replyOf <http://solidbench-server:3000/pods/00000002199023256081/comments/Philippines#274878069404>;
+          rdf:type snvoc:Comment;
+          snvoc:id ?commentId;
+          snvoc:content ?commentContent;
+          snvoc:creationDate ?commentCreationDate;
+          snvoc:hasCreator ?replyAuthor.
+        ?replyAuthor snvoc:id ?replyAuthorId;
+          snvoc:firstName ?replyAuthorFirstName;
+          snvoc:lastName ?replyAuthorLastName.
+        OPTIONAL {
+          ?messageCreator ((snvoc:knows/snvoc:hasPerson)|^(snvoc:knows/snvoc:hasPerson)) ?replyAuthor.
+          BIND("true"^^xsd:boolean AS ?replyAuthorKnowsOriginalMessageAuthorInner)
+        }
+        BIND(COALESCE(?replyAuthorKnowsOriginalMessageAuthorInner, "false"^^xsd:boolean) AS ?replyAuthorKnowsOriginalMessageAuthor)
+      }`;
+      const snvocPrefix = 'http://solidbench-server:3000/www.ldbc.eu/ldbc_socialnet/1.0/vocabulary/';
+      const rdfPrefix = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
+
+      const resp = createSimplePropertyObjectFromQuery(query);
+      const comment_philippines = 'http://solidbench-server:3000/pods/00000002199023256081/comments/Philippines#274878069404';
+      const expectedProperties = [
+        { property_iri: `${snvocPrefix}id`, object: DF.variable('messageId') },
+        { property_iri: `${snvocPrefix}hasCreator`, object: DF.variable('messageCreator') },
+        { property_iri: `${snvocPrefix}id`, object: DF.variable('messageCreatorId') },
+        { property_iri: `${snvocPrefix}replyOf`, object: DF.namedNode(comment_philippines) },
+        { property_iri: `${rdfPrefix}type`, object: DF.namedNode(`${snvocPrefix}Comment`) },
+        { property_iri: `${snvocPrefix}id`, object: DF.variable('commentId') },
+        { property_iri: `${snvocPrefix}content`, object: DF.variable('commentContent') },
+        { property_iri: `${snvocPrefix}creationDate`, object: DF.variable('commentCreationDate') },
+        { property_iri: `${snvocPrefix}hasCreator`, object: DF.variable('commentCreationDate') },
+        { property_iri: `${snvocPrefix}id`, object: DF.variable('replyAuthorId') },
+        { property_iri: `${snvocPrefix}firstName`, object: DF.variable('replyAuthorFirstName') },
+        { property_iri: `${snvocPrefix}lastName`, object: DF.variable('replyAuthorLastName') },
+        { property_iri: `${snvocPrefix}knows`, object: DF.blankNode() },
+        { property_iri: `${snvocPrefix}hasPerson`, object: DF.blankNode() },
+        { property_iri: `${snvocPrefix}knows`, object: DF.blankNode() },
+        { property_iri: `${snvocPrefix}hasPerson`, object: DF.blankNode() },
+
+      ].sort();
+
+      expect(resp.length).toBe(expectedProperties.length);
+      for (let i=0;i<resp.length;i++) {
+        expect(resp[i].property_iri).toStrictEqual(expectedProperties[i].property_iri);
+        expect(resp[i].object.termType).toStrictEqual(expectedProperties[i].object.termType);
+      }
+
     });
 
     it('should return an error given a malformed query', () => {
