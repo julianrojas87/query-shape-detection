2f8344d20271042b615e11473eb22ee887772510
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-03-29 13:32:14 +0100

    new packages exported

--- index.ts
+++ index.ts
@@ -2,3 +2,5 @@ export * from './lib/constant';
 export * from './lib/query';
 export * from './lib/shex';
 export * from './lib/aligment';
+export * from './lib/Shape';
+export * from './lib/Triple';
