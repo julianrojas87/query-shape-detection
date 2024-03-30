d509a05333bb11696ffb1789d61c09342c5455c2
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-03-30 06:54:51 +0100

    new shex constant added

--- lib/constant.ts
+++ lib/constant.ts
@@ -10,6 +10,17 @@ export const SHEX_SHAPE = DF.namedNode('http://www.w3.org/ns/shex#Shape');
 export const SHEX_EXPRESSIONS = DF.namedNode('http://www.w3.org/ns/shex#expressions');
 export const SHEX_CLOSED_SHAPE = DF.namedNode('http://www.w3.org/ns/shex#closed');
 export const SHEX_INVERSE = DF.namedNode('http://www.w3.org/ns/shex#inverse');
+export const SHEX_VALUE_EXPR = DF.namedNode('http://www.w3.org/ns/shex#valueExpr');
+export const SHEX_NODE_KIND = DF.namedNode('http://www.w3.org/ns/shex#nodeKind');
+export const SHEX_MAX = DF.namedNode('http://www.w3.org/ns/shex#max');
+export const SHEX_MIN = DF.namedNode('http://www.w3.org/ns/shex#min');
+export const SHEX_DATA_TYPE = DF.namedNode('http://www.w3.org/ns/shex#datatype');
+
+export const SHEX_LITERAL =DF.namedNode('http://www.w3.org/ns/shex#literal');
+export const SHEX_IRI = DF.namedNode('http://www.w3.org/ns/shex#iri');
+export const SHEX_BNODE = DF.namedNode('http://www.w3.org/ns/shex#bnode');
+export const SHEX_NON_LITERAL = DF.namedNode('http://www.w3.org/ns/shex#nonliteral');
+export const SHEX_VALUES = DF.namedNode('http://www.w3.org/ns/shex#values');
 
 export const TYPE_DEFINITION = DF.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#type');
 export const IRI_FIRST_RDF_LIST = DF.namedNode('http://www.w3.org/1999/02/22-rdf-syntax-ns#first');
