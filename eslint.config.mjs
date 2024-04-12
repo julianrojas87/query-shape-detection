4202a8704d7d149aee42ede30740029c98a13346
Bryan-Elliott Tam <bryanelliott.tam@ugent.be>
2024-04-12 09:34:37 +0200

    linter process modified

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.stylistic,
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off"
+    }
+  },
+  {
+    ignores: [
+      "**/*.js",
+      "**/*.mjs"
+    ]
+  }
+);
